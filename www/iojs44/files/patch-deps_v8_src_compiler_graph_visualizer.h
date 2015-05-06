ba45985e1aee2261db98e86a671e40f88f3cb084
Marcin Cieslak <saper@saper.info>
2015-05-06 08:01:37 +0000

    Fix http://code.google.com/p/v8/issues/detail?id=4079

---
+++ www/iojs44/files/patch-deps_v8_src_compiler_graph_visualizer.h
@@ -0,0 +1,10 @@
+--- deps/v8/src/compiler/graph-visualizer.h.orig	2015-05-06 07:49:33.000000000 +0000
++++ deps/v8/src/compiler/graph-visualizer.h	2015-05-06 07:49:44.000000000 +0000
+@@ -5,6 +5,7 @@
+ #ifndef V8_COMPILER_GRAPH_VISUALIZER_H_
+ #define V8_COMPILER_GRAPH_VISUALIZER_H_
+ 
++#include <stdio.h>
+ #include <iosfwd>
+ 
+ namespace v8 {
