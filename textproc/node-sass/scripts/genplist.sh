12ff0e75abef3d2b22c3a44ab26424b3e2ecbb8a
Marcin Cieslak <saper@saper.info>
2015-08-28 09:55:38 +0000

    Update node-sass to 36fb64a, hopeful 3.3.2

--- textproc/node-sass/scripts/genplist.sh
+++ textproc/node-sass/scripts/genplist.sh
@@ -1,3 +1,1 @@
-( ( cd `make -V STAGEDIR`/`make -V LOCALBASE` && find bin lib -type f -o -type l ) | sort | grep -v ^lib/node_modules/node-sass/vendor ;
- echo '@dir lib/node_modules/node-sass/src/libsass' ;
- echo '@dir lib/node_modules/node-sass/test/fixtures/spec' ) > pkg-plist
+( ( cd `make -V STAGEDIR`/`make -V LOCALBASE` && find bin lib -type f -o -type l ) | sort | grep -v ^lib/node_modules/node-sass/vendor ) > pkg-plist
