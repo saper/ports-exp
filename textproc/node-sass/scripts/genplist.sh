d85f0cd9abb2e6d948d36d7d762c88f8136ab8a8
Marcin Cieślak <saper@saper.info>
2016-04-23 23:11:17 +0000

    Add git dependency for node-sass

--- textproc/node-sass/scripts/genplist.sh
+++ textproc/node-sass/scripts/genplist.sh
@@ -1,6 +1,6 @@
 ( ( cd `make -V STAGEDIR`/`make -V LOCALBASE` && find bin lib -type f -o -type l ) | sort | grep -v ^lib/node_modules/node-sass/vendor;
-cat <<EOF
-@dir lib/node_modules/node-sass/src/libsass
-@dir lib/node_modules/node-sass/test/fixtures/spec
-EOF
+#cat <<EOF
+#@dir lib/node_modules/node-sass/src/libsass
+#@dir lib/node_modules/node-sass/test/fixtures/spec
+#EOF
 ) > pkg-plist
