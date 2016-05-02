3a5a97d45b24aa0e105e244c7e37d729a7e90892
Marcin Cieślak <saper@saper.info>
2016-05-02 11:37:28 +0000

    Add node 6.0.0 as www/node48

---
+++ www/node48/files/patch-deps_v8_src_base_platform_platform.h
@@ -0,0 +1,10 @@
+--- deps/v8/src/base/platform/platform.h.orig	2015-09-05 07:35:07 UTC
++++ deps/v8/src/base/platform/platform.h
+@@ -21,6 +21,7 @@
+ #ifndef V8_BASE_PLATFORM_PLATFORM_H_
+ #define V8_BASE_PLATFORM_PLATFORM_H_
+ 
++#include <stdio.h>
+ #include <cstdarg>
+ #include <string>
+ #include <vector>
